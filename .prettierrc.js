466b7e5e67d398e4e9066a35b63e4316a1ce6b12
Daniil Chubiy <daniil.chubiy@catenamedia.com>
2026-03-28 00:58:01 +0300

    remove extra files - fully sync with bonus codebase

--- .prettierrc.js
+++
@@ -1,6 +0,0 @@
-// Configuration file for the Prettier JS linting/formatting of @wordpress/scripts.
-
-module.exports = {
-	...require( '@wordpress/prettier-config' ),
-	printWidth: 250, // This overrides unwanted shortening of lines and extra word-wrapping, to some degree.
-};
